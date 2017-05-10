ae194545be179112b8e5ee0875e241f2a9d229d6
StefanieWang <wenping.wangyu@yahoo.com>
2017-05-10 10:31:19 +1000

    update script

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var simonGame = {
     init: function(){
         this.simonSequence = [];
         this.stepCount = 0;        
-        this.totalSteps = 3;
+        this.totalSteps = 20;
         this.err = false;
         this.playSequenceTimer = [];
         this.sequenceTimer = [];
@@ -34,8 +34,8 @@ var simonGame = {
     },
 
     audioPlay: function(index){
-        //var baseUrl = "https://s3.amazonaws.com/freecodecamp/";
-        var baseUrl = "audio/";
+        var baseUrl = "https://s3.amazonaws.com/freecodecamp/";
+        //var baseUrl = "audio/";
         var audio = ["simonSound1.mp3",
                      "simonSound2.mp3",
                      "simonSound3.mp3",
